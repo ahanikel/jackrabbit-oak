d10441677b32e1e7fe6a313d663e905ddd40bee3
Axel Hanikel <ahanikel@adobe.com>
2018-10-30 14:28:57 +0100

    OAK-7864 - Do logging in batches

--- oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/tool/LoggingNodeStateWrapper.java
+++ oak-segment-tar/src/main/java/org/apache/jackrabbit/oak/segment/tool/LoggingNodeStateWrapper.java
@@ -16,11 +16,12 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.jackrabbit.oak.segment.tool;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.spi.state.ChildNodeEntry;
 import org.apache.jackrabbit.oak.spi.state.NodeBuilder;
@@ -32,6 +33,7 @@ import org.slf4j.LoggerFactory;
 public class LoggingNodeStateWrapper extends NodeStateWrapper {
 
     private static final Logger LOG = LoggerFactory.getLogger(LoggingHook.class.getName() + ".reader");
+    private static final Queue<String> Q = new AutoFlushQueue();
 
     private final String path;
     private final boolean isRootNode;
@@ -161,17 +163,15 @@ public class LoggingNodeStateWrapper extends NodeStateWrapper {
     }
 
     private interface ReaderLogger {
+
         public void nodeRead(String n);
+
         public void propertyRead(String n, String p);
     }
 
-    private ReaderLogger log = new ReaderLogger() {
+    private final ReaderLogger log = new ReaderLogger() {
         @Override
         public void nodeRead(String n) {
-            if (n == null) {
-                n = "::: unknown :::";
-            }
-            log("n? " + LoggingHook.urlEncode(n));
         }
 
         @Override
@@ -183,7 +183,46 @@ public class LoggingNodeStateWrapper extends NodeStateWrapper {
         }
 
         private void log(String s) {
-            LOG.trace(System.currentTimeMillis() + " " + LoggingHook.urlEncode(Thread.currentThread().getName()) + " " + s);
+            Q.add(System.currentTimeMillis() + " " + LoggingHook.urlEncode(Thread.currentThread().getName()) + " " + s);
         }
     };
+
+    private static final class AutoFlushQueue extends ConcurrentLinkedQueue<String> {
+
+        private final Thread flushThread;
+
+        public AutoFlushQueue() {
+            this.flushThread = new Thread("ReadLogger") {
+                @Override
+                public void run() {
+                    for (;;) {
+                        try {
+                            if (LOG.isTraceEnabled()) {
+                                Thread.sleep(1000);
+                            } else {
+                                Thread.sleep(10000);
+                                continue;
+                            }
+                        } catch (InterruptedException ex) {
+                            return;
+                        }
+                        StringBuilder entries = new StringBuilder();
+                        String entry = AutoFlushQueue.this.poll();
+                        if (entry == null) {
+                            continue;
+                        }
+                        while (entry != null) {
+                            entries.append(entry);
+                            entries.append('\n');
+                            entry = AutoFlushQueue.this.poll();
+                        }
+                        entries.deleteCharAt(entries.length() - 1);
+                        LOG.trace(entries.toString());
+                    }
+                }
+            };
+            this.flushThread.setDaemon(true);
+            this.flushThread.start();
+        }
+    }
 }
