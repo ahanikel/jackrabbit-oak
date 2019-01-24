2f3d1e9eae55ca59b5f85f66bb754d13ce9ee65f
Axel Hanikel <ahanikel@adobe.com>
2019-01-24 19:18:29 +0100

    CommitLock testing

--- oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/scheduler/LockBasedSchedulerCheckpointTest.java
+++ oak-segment-tar/src/test/java/org/apache/jackrabbit/oak/segment/scheduler/LockBasedSchedulerCheckpointTest.java
@@ -39,7 +39,13 @@ import org.apache.jackrabbit.oak.spi.state.NodeState;
 import org.apache.jackrabbit.oak.stats.StatisticsProvider;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LockBasedSchedulerCheckpointTest {
+
+    private static final Logger log = LoggerFactory.getLogger(LockBasedSchedulerCheckpointTest.class);
+
     /**
      * OAK-3587 test simulates a timeout while trying to create a checkpoint,
      * then releases the lock and tries again
@@ -142,6 +148,68 @@ public class LockBasedSchedulerCheckpointTest {
         assertNotNull(retrieveCheckpoint(scheduler, cp0));
     }
 
+    @Test
+    public void testLongCommit() throws Exception {
+        final int blockTime = 30;
+        MemoryStore ms = new MemoryStore();
+        System.setProperty("oak.checkpoints.lockWaitTime", "2");
+        StatisticsProvider statsProvider = StatisticsProvider.NOOP;
+        SegmentNodeStoreStats stats = new SegmentNodeStoreStats(statsProvider);
+        final LockBasedScheduler scheduler = LockBasedScheduler.builder(ms.getRevisions(), ms.getReader(), stats)
+                .build();
+
+        final Semaphore semaphore = new Semaphore(0);
+
+        final Callable<Boolean> block = new Callable<Boolean>() {
+
+            @Override
+            public Boolean call() {
+                try {
+                    SECONDS.sleep(blockTime);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException("block has been interrupted");
+                }
+                return true;
+            }
+        };
+
+        Thread blockingCommitThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Commit commit = createBlockingCommit(scheduler, "blocking", "blocking", block);
+                    scheduler.schedule(commit);
+                    log.info("Blocking commit scheduled");
+                } catch (Exception e) {
+                    log.error(e.toString());
+                }
+            }
+        };
+
+        Thread followingCommitThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    Commit commit = createBlockingCommit(scheduler, "following", "following", () -> { return true; });
+                    scheduler.schedule(commit);
+                    log.info("Following commit scheduled");
+                } catch (Exception e) {
+                    log.error(e.toString());
+                }
+            }
+        };
+
+        log.info("Background thread starting");
+        blockingCommitThread.start();
+        log.info("Background thread started, starting following commit thread");
+        followingCommitThread.start();
+        log.info("Following thread started, acquiring semaphore");
+        semaphore.acquire();
+        log.info("Semaphore acquired");
+
+    }
+
     private NodeState retrieveCheckpoint(final Scheduler scheduler, final String checkpoint) {
         checkNotNull(checkpoint);
         NodeState cp = scheduler.getHeadNodeState().getChildNode("checkpoints").getChildNode(checkpoint)
@@ -166,6 +234,7 @@ public class LockBasedSchedulerCheckpointTest {
             public NodeState processCommit(NodeState before, NodeState after, CommitInfo info) {
                 try {
                     callable.call();
+                    log.info(property + "callable call finished");
                 } catch (Exception e) {
                     fail();
                 }
