22a0d21f0458141256d423ae83518996e64e2937
Axel Hanikel <ahanikel@adobe.com>
2019-06-14 14:55:58 +0200

    WIP

--- oak-store-zeromq/src/main/java/org/apache/jackrabbit/oak/store/zeromq/ZeroMQPropertyState.java
+++ oak-store-zeromq/src/main/java/org/apache/jackrabbit/oak/store/zeromq/ZeroMQPropertyState.java
@@ -22,11 +22,17 @@ package org.apache.jackrabbit.oak.store.zeromq;
 import org.apache.jackrabbit.oak.api.Blob;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Type;
+import org.apache.jackrabbit.oak.plugins.memory.StringBasedBlob;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.text.StringCharacterIterator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,60 +40,28 @@ import static org.apache.jackrabbit.oak.api.Type.*;
 
 public class ZeroMQPropertyState implements PropertyState {
 
+    private static final Logger log = LoggerFactory.getLogger(ZeroMQPropertyState.class);
+
     private final String name;
     private final Type type;
-    private final Object value;
+    private final List<String> values;
 
     public ZeroMQPropertyState(String name, String type, List<String> values) {
         this.name = name;
         this.type = Type.fromString(type);
-        if (
-                this.type.equals(STRING) ||
-                this.type.equals(DATE) ||
-                this.type.equals(NAME) ||
-                this.type.equals(PATH) ||
-                this.type.equals(REFERENCE) ||
-                this.type.equals(WEAKREFERENCE) ||
-                this.type.equals(URI)
-        ) {
-            this.value = values.get(0);
-        } else if (
-                this.type.equals(STRINGS) ||
-                this.type.equals(DATES) ||
-                this.type.equals(NAMES) ||
-                this.type.equals(PATHS) ||
-                this.type.equals(REFERENCES) ||
-                this.type.equals(WEAKREFERENCES) ||
-                this.type.equals(URIS)
-        ) {
-            this.value = values;
-        } else if (this.type.equals(BINARY)) {
-            this.value = values;
-        } else if (this.type.equals(BINARIES)) {
-            this.value = values;
-        } else if (this.type.equals(LONG)) {
-            this.value = Long.valueOf(values.get(0));
-        } else if (this.type.equals(LONGS)) {
-            this.value = new ArrayList<Long>();
-            values.stream().map(Long::valueOf).forEach(((ArrayList<Long>) this.value)::add);
-        } else if (this.type.equals(DOUBLE)) {
-            this.value = Double.valueOf(values.get(0));
-        } else if (this.type.equals(DOUBLES)) {
-            this.value = new ArrayList<Double>();
-            values.stream().map(Double::valueOf).forEach(((ArrayList<Double>) this.value)::add);
-        } else if (this.type.equals(BOOLEAN)) {
-            this.value = Boolean.valueOf(values.get(0));
-        } else if (this.type.equals(BOOLEANS)) {
-            this.value = new ArrayList<Boolean>();
-            values.stream().map(Boolean::valueOf).forEach(((ArrayList<Boolean>) this.value)::add);
-        } else if (this.type.equals(DECIMAL)) {
-            this.value = new BigDecimal(values.get(0));
-        } else if (this.type.equals(DECIMALS)) {
-            this.value = new ArrayList<BigDecimal>();
-            values.stream().map(v -> new BigDecimal(v)).forEach(((ArrayList<BigDecimal>) this.value)::add);
-        } else {
-            throw new IllegalArgumentException(type);
+        this.values = values;
+    }
+
+    public ZeroMQPropertyState(PropertyState ps) {
+        this.values = new ArrayList<>();
+        this.name = ps.getName();
+        this.type = ps.getType();
+        if (ps.isArray()) {
+            for (int i = 0; i < ps.count(); ++i) {
+                this.values.add(valueToString(type.getBaseType(), ps.getValue(type.getBaseType(), i)));
+            }
         }
+        this.values.add(valueToString(type, ps.getValue(type)));
     }
 
     @Override
@@ -107,38 +81,68 @@ public class ZeroMQPropertyState implements PropertyState {
 
     @Override
     public <T> T getValue(Type<T> type) {
-        return (T) value;
+        if (type.isArray()) {
+            if (
+                    type.equals(STRINGS) ||
+                    type.equals(DATES) ||
+                    type.equals(NAMES) ||
+                    type.equals(PATHS) ||
+                    type.equals(REFERENCES) ||
+                    type.equals(WEAKREFERENCES) ||
+                    type.equals(URIS)
+            ) {
+                return (T) values;
+            } else {
+                throw new IllegalStateException(type.toString());
+            }
+        } else {
+            return getValue(type, 0);
+        }
     }
 
     @Override
     public <T> T getValue(Type<T> type, int index) {
-        if (type.isArray()) {
+        if (
+                type.equals(STRING) ||
+                type.equals(DATE) ||
+                type.equals(NAME) ||
+                type.equals(PATH) ||
+                type.equals(REFERENCE) ||
+                type.equals(WEAKREFERENCE) ||
+                type.equals(URI)
+        ) {
+            return (T) values.get(index);
+        } else if (type.equals(BINARY)) {
+            return (T) blobFromString(values.get(index));
+        } else if (type.equals(LONG)) {
+            return (T) Long.valueOf(values.get(index));
+        } else if (type.equals(DOUBLE)) {
+            return (T) Double.valueOf(values.get(index));
+        } else if (type.equals(BOOLEAN)) {
+            return (T) Boolean.valueOf(values.get(index));
+        } else if (type.equals(DECIMAL)) {
+            return (T) new BigDecimal(values.get(index));
+        } else {
             throw new IllegalArgumentException(type.toString());
         }
-        return ((ArrayList<T>) this.value).get(index); // TODO: see doc
     }
 
     @Override
     public long size() {
         // TODO: not sure if that's correct
-        if (type.isArray()) {
-            return ((ArrayList) value).size();
-        } else {
-            return 1;
-        }
+        return values.get(0).length();
     }
 
     @Override
     public long size(int index) {
-        // TODO:
-        return 1;
+        return values.get(index).length();
     }
 
     @Override
     public int count() {
         // TODO: not sure if that's correct
         if (type.isArray()) {
-            return ((ArrayList) value).size();
+            return values.size();
         } else {
             return 1;
         }
@@ -149,26 +153,28 @@ public class ZeroMQPropertyState implements PropertyState {
         sb.append(" <");
         sb.append(getType());
         sb.append("> ");
-        if (getType() == BINARY) {
-            sb.append("= ");
-            final Blob blob = getValue(BINARY);
-            appendBlob(sb, blob);
-        } else if (getType() == BINARIES) {
+        if (isArray()) {
             sb.append("= [");
-            getValue(BINARIES).forEach((Blob b) -> {
-                appendBlob(sb, b);
-                sb.append(',');
-            });
-            replaceOrAppendLastChar(sb, ',', ']');
-        } else if (isArray()) {
-            sb.append("= [");
-            getValue(STRINGS).forEach((String s) -> {
-                sb.append(safeEncode(s));
+            values.forEach((String s) -> {
+                if (type.equals(BINARIES)) {
+                    sb.append(s);
+                } else {
+                    sb.append(safeEncode(s));
+                }
                 sb.append(',');
             });
             replaceOrAppendLastChar(sb, ',', ']');
         } else {
-            sb.append("= ").append(safeEncode(getValue(STRING)));
+            try {
+                sb.append("= ");
+                if (type.equals(BINARY)) {
+                    sb.append(values.get(0));
+                } else {
+                    sb.append(safeEncode(values.get(0)));
+                }
+            } catch (ClassCastException e) {
+                log.error(e.toString());
+            }
         }
         return sb;
     }
@@ -195,11 +201,78 @@ public class ZeroMQPropertyState implements PropertyState {
         }
     }
 
+    private static Blob blobFromString(String s) {
+        return new Blob() {
+
+            @Override
+            public @NotNull InputStream getNewStream() {
+                return new InputStream() {
+
+                    StringCharacterIterator it = new StringCharacterIterator(s);
+
+                    private int hexCharToInt(char c) {
+                        return Character.isDigit(c) ? c - '0' : c - 'A' + 10;
+                    }
+
+                    @Override
+                    public int read() throws IOException {
+                        final int c = hexCharToInt(it.next());
+                        final int d = hexCharToInt(it.next());
+                        final int ret = c << 4 | d;
+                        return ret;
+                    }
+                };
+            }
+
+            @Override
+            public long length() {
+                return s.length() / 2;
+            }
+
+            @Override
+            public @Nullable String getReference() {
+                return null;
+            }
+
+            @Override
+            public @Nullable String getContentIdentity() {
+                return null;
+            }
+        };
+    }
+
     private static String safeEncode(String value) {
         try {
             return SafeEncode.safeEncode(value);
         } catch (UnsupportedEncodingException e) {
             return "ERROR: " + e;
+        }
+    }
+
+    private static <T> String valueToString(Type<T> from, T v) {
+        if (
+                from.equals(STRING) ||
+                from.equals(DATE) ||
+                from.equals(NAME) ||
+                from.equals(PATH) ||
+                from.equals(REFERENCE) ||
+                from.equals(WEAKREFERENCE) ||
+                from.equals(URI)
+        ) {
+            return (String) v;
+        } else if (from.equals(BINARY)) {
+            final StringBuilder sb = new StringBuilder();
+            appendBlob(sb, (Blob) v);
+            return sb.toString();
+        } else if (
+                from.equals(LONG) ||
+                from.equals(DOUBLE) ||
+                from.equals(BOOLEAN) ||
+                from.equals(DECIMAL)
+        ) {
+            return v.toString();
+        } else {
+            throw new IllegalArgumentException(from.toString());
         }
     }
 }
