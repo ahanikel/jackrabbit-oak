1bde8c01e1bd4ca83c6c36f29719a7bc4f738278
Axel Hanikel <ahanikel@adobe.com>
2021-05-04 15:37:10 +0200

    Reduce number of threads, save some resources

--- oak-store-zeromq/src/main/java/org/apache/jackrabbit/oak/store/zeromq/ZeroMQBlob.java
+++ oak-store-zeromq/src/main/java/org/apache/jackrabbit/oak/store/zeromq/ZeroMQBlob.java
@@ -33,7 +33,7 @@ public class ZeroMQBlob implements Blob {
         private volatile File file;
         private volatile String reference;
         private final InputStream is;
-        private static final ExecutorService readerThreads = Executors.newFixedThreadPool(50);
+        private static final ExecutorService readerThreads = Executors.newFixedThreadPool(10);
         private final CountDownLatch countDownLatch;
 
         InputStreamFileSupplier(@NotNull InputStream is) {
