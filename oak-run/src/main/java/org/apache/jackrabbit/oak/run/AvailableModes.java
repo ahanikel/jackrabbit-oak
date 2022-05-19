9bb05cde1976fcdf0f15f7f4b36e8cd98b06c67c
Axel Hanikel <ahanikel@adobe.com>
2022-05-19 14:08:29 +0200

    Communication hub pt 1

--- oak-run/src/main/java/org/apache/jackrabbit/oak/run/AvailableModes.java
+++ oak-run/src/main/java/org/apache/jackrabbit/oak/run/AvailableModes.java
@@ -25,6 +25,7 @@ import org.apache.jackrabbit.oak.exporter.NodeStateExportCommand;
 import org.apache.jackrabbit.oak.index.IndexCommand;
 import org.apache.jackrabbit.oak.run.commons.Command;
 import org.apache.jackrabbit.oak.run.commons.Modes;
+import org.apache.jackrabbit.oak.simple.CommHubCommand;
 import org.apache.jackrabbit.oak.simple.ImportToSimpleCommand;
 import org.apache.jackrabbit.oak.simple.SerialiseNodeStoreCommand;
 import org.apache.jackrabbit.oak.simple.SimpleBlobReaderServiceCommand;
@@ -69,6 +70,7 @@ public final class AvailableModes {
             .put(SerialiseNodeStoreCommand.NAME, new SerialiseNodeStoreCommand())
             .put("simple-blob-reader-service", new SimpleBlobReaderServiceCommand())
             .put("simple-blob-writer-service", new SimpleNodeStateWriterServiceCommand())
+            .put(CommHubCommand.NAME, new CommHubCommand())
             .put("tarmkdiff", new FileStoreDiffCommand())
             .put(ThreadDumpCommand.THREADDUMP, new ThreadDumpCommand())
             .put("tika", new TikaCommand())
